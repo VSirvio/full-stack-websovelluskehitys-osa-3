97b81fa769f0b15fd1b7ced871e79aff5190f376
Ville Sirviö <VSirvio@users.noreply.github.com>
2024-08-25 11:19:20 +0300

    Tehtävä 3.9

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
 
 morgan.token('body', (request, response) => JSON.stringify(request.body))
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] ' +
                '- :response-time ms :body'))
